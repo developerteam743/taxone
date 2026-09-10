71970eb68d2515af5c518dd1df1f5e9cbea32382
developerteam743 <developerteam743@gmail.com>
2026-09-10 10:33:49 +0530

    Delete apps directory

--- apps/web/components/SimpleShell.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { ReactNode } from 'react';
-import Shell from '@/components/Shell';
-
-export default function SimpleShell({ title, children }: { title: string; children: ReactNode }) {
-  return <Shell title={title}>{children}</Shell>;
-}
